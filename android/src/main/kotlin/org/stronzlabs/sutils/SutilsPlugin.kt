69710f389c3adab55d5050eee5c021c8581d8984
Bonfra04 <bonfract@gmail.com>
2024-08-29 11:00:50 +0200

    fixed null reference to package managr

--- android/src/main/kotlin/org/stronzlabs/sutils/SutilsPlugin.kt
+++ android/src/main/kotlin/org/stronzlabs/sutils/SutilsPlugin.kt
@@ -1,5 +1,6 @@
 package org.stronzlabs.sutils
 
+import android.content.pm.PackageManager
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -10,19 +11,24 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 class SUtilsPlugin: FlutterPlugin, MethodCallHandler {
     private lateinit var channel : MethodChannel
+    private var packageManager : PackageManager? = null
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "org.stronzlabs.sutils/utils")
         channel.setMethodCallHandler(this)
+
+        packageManager = flutterPluginBinding.applicationContext.packageManager
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+
+        packageManager = null
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "isAndroidTV") {
-            val isTV : Boolean = packageManager?.hasSystemFeature(PackageManager.FEATURE_TELEVISION) ?: false
+            val isTV : Boolean = packageManager?.hasSystemFeature(PackageManager.FEATURE_LEANBACK) ?: false
             result.success(isTV)
         } else
             result.notImplemented()
