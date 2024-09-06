3419ec3c5db10b5aebe16f4d6d7d2ee297750a8e
Bonfra04 <bonfract@gmail.com>
2024-09-07 00:06:43 +0200

    support for post requests

--- lib/utils/simple_http.dart
+++ lib/utils/simple_http.dart
@@ -7,7 +7,7 @@ import 'package:http/io_client.dart';
 final class HTTP {
     HTTP._();
     
-    static Future<Response> _get(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
+    static Future<Response> _request(dynamic url, String method, {Map<String, String>? headers, Map<String, String>? body, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
         assert(url is String || url is Uri);
         headers ??= {};
         if(!headers.containsKey("User-Agent"))
@@ -18,9 +18,11 @@ final class HTTP {
 
         for (int i = 0; i < maxRetries; i++) {
             try {
-                Request req = Request("GET", url is Uri ? url : Uri.parse(url));
+                Request req = Request(method, url is Uri ? url : Uri.parse(url));
                 req.headers.addAll(headers);
                 req.followRedirects = followRedirects;
+                if (body != null)
+                    req.bodyFields = body;
 
                 IOClient ioClient = IOClient(client);
                 StreamedResponse response = await ioClient.send(req);
@@ -33,6 +35,11 @@ final class HTTP {
 
         throw Exception("Failed to fetch resource after ${maxRetries} retries");
     }
+
+    static Future<Response> _get(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
+        return _request(url, "GET", headers: headers, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
+    }
+
 
     static Future<Uint8List> getRaw(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
         Response response = await _get(url, headers: headers, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
@@ -47,30 +54,7 @@ final class HTTP {
     }
 
     static Future<Response> _head(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
-        assert(url is String || url is Uri);
-        headers ??= {};
-        if(!headers.containsKey("User-Agent"))
-            headers["User-Agent"] = "StronzLabs";
-
-        HttpClient client = HttpClient();
-        client.connectionTimeout = timeout;
-
-        for (int i = 0; i < maxRetries; i++) {
-            try {
-                Request req = Request("HEAD", url is Uri ? url : Uri.parse(url));
-                req.headers.addAll(headers);
-                req.followRedirects = followRedirects;
-
-                IOClient ioClient = IOClient(client);
-                StreamedResponse response = await ioClient.send(req);
-                return Response.fromStream(response);
-            } catch (e) {
-                if (i == maxRetries - 1)
-                    rethrow;
-            }
-        }
-
-        throw Exception("Failed to fetch resource after ${maxRetries} retries");
+        return _request(url, "HEAD", headers: headers, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
     }
 
     static Future<Map<String, String>> head(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
@@ -86,5 +70,21 @@ final class HTTP {
     static Future<int> status(dynamic url, {Map<String, String>? headers, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
         Response response = await _head(url, headers: headers, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
         return response.statusCode;
+    }
+
+    static Future<Response> _post(dynamic url, {Map<String, String>? headers, Map<String, String>? body, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
+        return _request(url, "POST", headers: headers, body: body, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
+    }
+
+    static Future<Uint8List> postRaw(dynamic url, {Map<String, String>? headers, Map<String, String>? body, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
+        Response response = await _post(url, headers: headers, body: body, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries);
+        if (response.statusCode != 200)
+            return Uint8List(0);
+
+        return response.bodyBytes;
+    }
+
+    static Future<String> post(dynamic url, {Map<String, String>? headers, Map<String, String>? body, bool followRedirects = true, Duration? timeout, int maxRetries = 1}) async {
+        return (await _post(url, headers: headers, body: body, followRedirects: followRedirects, timeout: timeout, maxRetries: maxRetries)).body;
     }
 }
