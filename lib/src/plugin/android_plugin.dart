fc52b95205125c0d84d45eff692b1d8df7451988
Bonfra04 <bonfract@gmail.com>
2025-01-12 19:09:32 +0100

    moved device orientation and decorations code in here

--- lib/src/plugin/android_plugin.dart
+++ lib/src/plugin/android_plugin.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sutils/utils.dart';
 
+void _setupOrientationAndOverlays() {
+    SystemChrome.setPreferredOrientations([
+        if(EPlatform.isTV || EPlatform.isTablet) ...[
+            DeviceOrientation.landscapeLeft,
+            DeviceOrientation.landscapeRight
+        ] else
+            DeviceOrientation.portraitUp
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+    ));
+}
+
 void registerWith() {
     Future.delayed(Duration.zero, () async {
         WidgetsFlutterBinding.ensureInitialized();
         MethodChannel channel = MethodChannel('org.stronzlabs.sutils/utils');
         EPlatform.isAndroidTV = await channel.invokeMethod<bool>('isAndroidTV') ?? false;
+        _setupOrientationAndOverlays();
     });
 }
