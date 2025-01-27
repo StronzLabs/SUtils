87c3727ef3ead1adacd6df177cb01174e21587db
Bonfra04 <bonfract@gmail.com>
2025-01-27 14:50:32 +0100

    actually exporting the new things

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,8 +4,10 @@ export 'src/utils/duration.dart';
 export 'src/utils/expanded_platform.dart';
 export 'src/utils/fullscreen.dart';
 export 'src/utils/resource_manager.dart';
-export 'src/utils/stream_listener.dart';
 export 'src/utils/safe_wakelock.dart';
+export 'src/utils/stream_listener.dart';
+export 'src/utils/string.dart';
+export 'src/utils/uint8list.dart';
 
 export 'src/http/simple_http.dart';
 export 'src/http/http_chain.dart';
